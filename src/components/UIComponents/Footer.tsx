28841bbd69383dd7716c1b929e6fef0d26a088f3
harii84 <99866727+harii84@users.noreply.github.com>
2022-08-11 15:02:11 +0530

    Notes Mini

---
+++ src/components/UIComponents/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer(): JSX.Element {
+	return (
+		<footer>
+			<p>
+				Created by <a className="rising-background" href="https://instagram.com/harii.84" target="_blank" rel="noreferrer noopener">Harii.84</a>
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;
